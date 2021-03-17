06b66995ad46d25bccf78e718854a4bfe8c91b50
Nicholas Kaaz Omahen <nomahen@ucsc.edu>
2021-03-17 11:14:27 -0500

    Adding constants.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+## declare constants
+pc_to_cm   = 3.086e18    # parsec in centimeters
+au_to_cm   = 1.496e13    # astronomical unit in centimeters
+smass_to_g = 1.988435e33 # solar mass in grams
+c          = 2.998e10    # speed of light, cm/s
+G          = 6.674e-8    # gravitational constant, cgs
+sig_sb     = 5.6704e-5   # stefan-boltzmann constant, cgs
+k_b        = 1.3806e-16  # boltzmann constant, cgs 
+mH         = 1.673e-24   # hydrogen mass, cgs
+yr_to_sec  = 3.54e7      # convert years to seconds
